ec5a06cbddb0ec95b3c80be0acfee225d6b9c2d4
BechirGB <bechirguerriche@gmail.com>
2025-10-15 12:07:41 +0100

    first commit

--- backend/models/User.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const UserSchema = new mongoose.Schema({
-    UserName:{type:String,required:true},
-    Email:{type:String,required:true,unique:true},
-    Password:{type:String,required:true},
-});
-
-export default mongoose.model("User", UserSchema); 
